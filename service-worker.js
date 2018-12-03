e46647eefac0d9c643dcefc612a24128b3202d11
Shawn Naquin <shawn.naquin@gmail.com>
2018-12-03 17:02:58 -0600

    new build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be65bcf42a4fcf13283a76012e8ef1a0.js"
+  "/precache-manifest.2d64fa49b10e2b9c9b54a93b4f3c3611.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "shawn-portfolio-2018"});
