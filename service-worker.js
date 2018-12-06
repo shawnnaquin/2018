92c41ea1145c609260ebf88ac046f86ec23b7d1e
shawnnaquin <shawn.naquin@gmail.com>
2018-12-06 15:10:44 -0600

    build

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
-importScripts("/precache-manifest.93182d40307112d686ddc576596ad123.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-
-if (workbox) {
-    console.log(`Workbox is loaded`);
-    // workbox.precaching.precacheAndRoute(self.__precacheManifest);
-    // self.__precacheManifest = [].concat(self.__precacheManifest || []);
-    workbox.precaching.suppressWarnings();
-    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+try {
+  importScripts("/precache-manifest.63ff4a34cadf7d6fc67b2f40efae4fcd.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+  if (workbox) {
+      console.log(`Workbox is loaded`);
+      // workbox.precaching.precacheAndRoute(self.__precacheManifest);
+      // self.__precacheManifest = [].concat(self.__precacheManifest || []);
+      workbox.precaching.suppressWarnings();
+      workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+  }
 }
-
-else {
-
+catch(error) {
     console.log(`Workbox didn't load`);
-
+  // expected output: SyntaxError: unterminated string literal
+  // Note - error messages will vary depending on browser
 }
 
 self.addEventListener("message", (e)=>{
